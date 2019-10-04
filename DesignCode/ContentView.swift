96aedd89ead9dc958e8479ce26bef6b37184ee6c
Brett Chapin <blackroseangel@gmail.com>
2019-10-04 09:19:11 -0400

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Brett Chapin on 10/4/19.
+//  Copyright © 2019 Black Rose Productions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
